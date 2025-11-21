cf239c857f46bd2cc2ef5d5e0b15c305f93a0cf2
Sohan Rathod <68809812+SohanR09@users.noreply.github.com>
2025-11-21 18:56:48 +0530

    lets party

---
+++ DP/Lets_party.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solve(int A) {
+        int mod = 10003;
+        int[] dp = new int[A + 1];
+
+        dp[0] = 1;
+        dp [1] = 1;
+
+        for (int i = 2; i <= A; i++) {
+            dp[i] = (dp[i-1] + (i-1) * dp[i-2]) % mod;
+        }
+
+        return dp[A];
+    }
+}
