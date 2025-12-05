fc79db48a20f821c6d6d10faddf79ec7822f232f
Sohan Rathod <68809812+SohanR09@users.noreply.github.com>
2025-12-05 21:52:53 +0530

    infix to postfix

---
+++ Stacks_&_Queue/Infix_to_postfix.java
@@ -0,0 +1,46 @@
+public class Solution {
+    private int getPrecedence(char operator){
+        switch (operator) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+    public String solve(String A) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : A.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                result.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                }
+            } else {
+                while (!stack.isEmpty() && getPrecedence(c) <= getPrecedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            if (stack.peek() == '(') return "Invalid Expression";
+            result.append(stack.pop());
+        }
+
+        return result.toString();
+    }
+}
