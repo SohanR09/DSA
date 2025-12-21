b18253d58d8853d955742320066508c2f00b0c9d
Sohan Rathod <68809812+SohanR09@users.noreply.github.com>
2025-12-21 23:08:14 +0530

    single number II

---
+++ Arrays/Bit_manipulation/Single_number_II.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int singleNumber(final List<Integer> A) {
+        int ans = 0;
+        int n = A.size();
+        for(int i = 0; i < 32; i++){
+            int count = 0;
+            for(int j = 0; j < n; j++){
+                if((A.get(j) & (1 << i)) != 0) count++;
+            }
+            if(count % 3 != 0 ){
+                ans = ans | (1 << i);
+            }
+        }
+        return ans;
+    }
+}
